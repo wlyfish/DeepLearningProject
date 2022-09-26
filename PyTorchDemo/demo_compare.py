9319c587a28f13016487fcc00fffa80e3fa336c2
wly <2825024660@qq.com>
2022-09-26 09:07:00 +0800

    PyTorch compare

--- PyTorchDemo/demo_compare.py
+++ PyTorchDemo/demo_compare.py
@@ -6,6 +6,8 @@
 @create_time = 2022/9/21 20:48
 """
 import torch
+import numpy as np
+
 
 a = torch.rand(2, 3)
 b = torch.rand(2, 3)
@@ -29,10 +31,30 @@ print(a.shape)
 print(torch.sort(a))
 print(torch.sort(a, dim=0, descending=True))
 
-print("############################")
+print("############# sort ###############")
 
 b = torch.tensor([[1, 4, 4, 3, 5],
                   [2, 3, 1, 3, 5]])
 print(b)
 print(b.shape)
-print(torch.sort(b, dim=0, descending=False))
+print(torch.sort(b, dim=1, descending=False))
+
+print("############# topk ###############")
+a = torch.tensor([[2, 4, 3, 1, 5],
+                  [2, 3, 5, 1, 4]])
+print(a)
+print(torch.topk(a, k=2, dim=1))
+
+print(torch.kthvalue(a, k=2, dim=0))
+print(torch.kthvalue(a, k=2, dim=1))
+
+a = torch.rand(2, 3)
+print(a)
+print('a.isfinite = {}'.format(torch.isfinite(a)))
+print('a/0 isfinite = {}'.format(torch.isfinite(a/0)))
+print('a/0 isinf = {}'.format(torch.isinf(a/0)))
+print('a isnan = {}'.format(torch.isnan(a)))
+
+a = torch.tensor([1, 2, np.nan])
+print(a)
+print('a isnan = {}'.format(torch.isnan(a)))
